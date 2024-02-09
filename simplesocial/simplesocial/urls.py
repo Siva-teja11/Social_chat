59bdc9385183441e36137612a6a18bd44e60b866
Siva-teja11 <sivatejaadari123@gmail.com>
2024-02-09 11:45:35 +0530

    login and sign up page created and added

--- simplesocial/simplesocial/urls.py
+++ simplesocial/simplesocial/urls.py
@@ -19,6 +19,11 @@ from django.urls import path, include
 from . import views
 
 urlpatterns = [
+    path('',views.Homepage.as_view(),name='home'),
     path('admin/', admin.site.urls),
+    path('test/', views.TestPage.as_view(), name="test"),
+    path('thanks/', views.ThanksPage.as_view(), name="thanks"),
+    path('accounts/', include("accounts.urls", namespace="accounts")),
+    path('accounts/', include("django.contrib.auth.urls")),
     
 ]
